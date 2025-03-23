9df27391cc2fbf7ee338617c0c8298cf10c83bdb
Parth-RK <2021bcs119@sggs.ac.in>
2025-03-23 22:11:35 +0530

    Added a generalized neural network library(nn.h) and its xor(xor.c) implementation

---
+++ neural_network/neural_network.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+
+
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ neural_network/nn.c
@@ -0,0 +1,29 @@
+#define NN_IMPLEMENTATION
+#include "nn.h"
+#include <time.h>
+
+int main(){
+    srand(time(0));
+    Mat a = mat_alloc(1, 2);
+    mat_rand(a, 5, 10);
+
+
+    float id_data[4] = {
+        1, 0,
+        0, 1,
+    };
+    Mat b = {.rows = 2, .cols = 2, .es = id_data};
+    // mat_rand(b, 5, 10);
+    Mat dst = mat_alloc(1, 2);
+
+    mat_print(a, "A");
+    mat_print(b, "B");
+    printf("----------------------------------\n");
+    mat_dot(dst, a, b);
+    mat_print(dst, "C = A * B");
+
+    mat_add(dst, a);
+    mat_print(dst, "A + C");
+    
+    return 0;
+}
